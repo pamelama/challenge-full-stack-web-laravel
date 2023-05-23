4eb655c7370a78ef321c1ef01cf4596448f97231
Pamela Anizio <aniziopamela@gmail.com>
2023-05-23 18:56:59 -0300

    Frontend com Vuetify

---
+++ frontend/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-app>
+    <!-- <v-main>
+      <HelloWorld />
+    </v-main> -->
+    <Home />
+  </v-app>
+</template>
+
+<script setup>
+  import HelloWorld from '@/components/HelloWorld.vue'
+import Home from './pages/Home.vue';
+</script>
---
+++ frontend/src/pages/Home.vue
@@ -0,0 +1,37 @@
+<script setup>
+</script>
+<template>
+  <v-layout>
+    
+    <v-navigation-drawer permanent>
+      <v-list  class="text-center">
+        <v-list-item>
+      <v-img src="./../assets/logo-grupoa.png" alt="Grupo A" :width="100" class="mx-auto mb-8"></v-img>
+    </v-list-item>
+        <v-subheader class="font-weight-bold">Módulo Acadêmico</v-subheader>
+        <v-list-item title="Alunos" class="cursor-pointer"></v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar title="Consulta de Alunos" class="bg-red"></v-app-bar>
+
+    
+
+    <v-main style="min-height: 300px;">
+     
+      <v-container>
+      <v-form>
+        <v-row>
+          <v-col cols="8">
+            <v-text-field label="Digite sua busca"></v-text-field>
+          </v-col>
+          <v-col cols="2">
+            <v-btn size="large">Button</v-btn>
+          </v-col>
+        </v-row>
+      </v-form>
+    </v-container>
+    </v-main>
+  </v-layout>
+
+</template>
